fff4fcea699cb73d273ee00d599b5305ee6871bc
Antoinette Clare <AClare@dcm-fvfgn05rq05q.dunelmmill.com>
2022-03-01 15:55:10 +0000

    adding main

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.22.0"
+    }
+  }
+}
+
+provider "snowflake" {
+  alias = "sys_admin"
+  role  = "SYSADMIN"
+}
+
+resource "snowflake_database" "db" {
+  provider = snowflake.sys_admin
+  name     = "TF_DEMO"
+}
+
+resource "snowflake_warehouse" "warehouse" {
+  provider       = snowflake.sys_admin
+  name           = "TF_DEMO"
+  warehouse_size = "large"
